ede3ee762e31041174ad6700991c886f7f8e3039
rohithagaram <rohithagaram@gmail.com>
2021-07-19 00:09:03 +0530

    removed the warp code

--- Hmatrix_estimation_Ransac.m
+++ Hmatrix_estimation_Ransac.m
@@ -18,38 +18,5 @@ img1_points = matchedPoints1.Location;
 img2_points = matchedPoints2.Location;
 
 %% Estimating Homography using RANSAC
-A_ransac = estimateTransformRANSAC(img1_points,img2_points)
+H_ransac = estimateTransformRANSAC(img1_points,img2_points)
 
-%% Applying Homography Transformation Matrix directly to RGB Image
-img2rgb = im2double(imread(path_to_image2));
-im2_transformed = transformImage(img2rgb,A_ransac);
-%imshow(im2_transformed)
-
-%% Expanding the image1 to match size of image2
-img1rgb = im2double(imread('1.jpg'));
-im1_size = size(img1rgb);
-im2_transformed_size = size(im2_transformed);
-padsize = im2_transformed_size-im1_size;
-img1_expanded = padarray(img1rgb,padsize,'post');
-%figure
-%imshow(img1_expanded)
-
-%% Blending images with RAMP
-[x_overlap,y_overlap]=ginput(2); %Taking user input for taking two points for starting and ending of ramp
-overlapleft = round(x_overlap(1));
-overlapright = round(x_overlap(2));
-diff = overlapright-overlapleft;
-ramp = [zeros(1,overlapleft-1),0:1/diff:1,ones(1,im2_transformed_size(2)-overlapright)];
-im2_blend = im2_transformed .* repmat(ramp,im2_transformed_size(1),1);
-%figure
-%imshow(im2_blend)
-
-ramp_for_im1 = 1 - ramp;
-im1_blend = img1_expanded .* repmat(ramp_for_im1,im2_transformed_size(1),1);
-%figure
-%imshow(im1_blend)
-
-%% Creating the panorama
-impanorama = im1_blend+im2_blend;
-figure
-imshow(impanorama)
